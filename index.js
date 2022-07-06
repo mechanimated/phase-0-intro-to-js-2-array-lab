a92b158ea22fb526e497552f2a6f940064f47c37
Liz Boika <mdaicdxl@gmail.com>
2022-07-05 20:55:34 -0400

    commit tree ffdc6cb204f5c86274c78392e919f718554a11bf

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+};
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const appendCat = [...cats]
+    appendCat.push("Broom");
+    return appendCat
+}
+
+function prependCat() {
+    const prependCat = [...cats]
+    prependCat.unshift("Arnold");
+    return prependCat
+}
+
+function removeLastCat() {
+    const removeLastCat = [...cats]
+    removeLastCat.pop();
+    return removeLastCat
+}
+
+function removeFirstCat() {
+    const removeFirstCat = [...cats]
+    removeFirstCat.shift();
+    return removeFirstCat
+}
